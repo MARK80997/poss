c70e1f895f849495726f7f09e0f3b30b285c4e69
olas <merida.marcos.137@gmail.com>
2023-10-31 12:16:56 -0400

    Actualización del archivo dataBase

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => 'mm80997',
-        'database' => 'pos',
+        'database' => 'ferretera',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
